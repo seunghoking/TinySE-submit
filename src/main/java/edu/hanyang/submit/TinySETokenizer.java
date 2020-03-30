a391d54aa4826eb3ff07bb48d046faeeb6b9ef6f
현승호 <hyeonseungho@hyeonseunghoui-MacBookAir.local>
2020-03-30 19:17:18 +0900

    2020.3.30 update Tokenizer

--- src/main/java/edu/hanyang/submit/TinySETokenizer.java
+++ src/main/java/edu/hanyang/submit/TinySETokenizer.java
@@ -1,19 +1,53 @@
 package edu.hanyang.submit;
 
+import java.io.IOException;
+import java.io.StringReader;
+import java.util.ArrayList;
 import java.util.List;
+
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.TokenStream;
+import org.apache.lucene.analysis.core.SimpleAnalyzer;
+import org.apache.lucene.analysis.tokenattributes.CharTermAttribute;
+import org.tartarus.snowball.ext.PorterStemmer;
 
 import edu.hanyang.indexer.Tokenizer;
 
 public class TinySETokenizer implements Tokenizer {
 
+	private SimpleAnalyzer analyzer;
+	private PorterStemmer stemmer;
+	
 	public void setup() {
+		analyzer = new SimpleAnalyzer();
+		stemmer = new PorterStemmer();
 	}
 
 	public List<String> split(String text) {
-		return null;
+		List<String> list = new ArrayList<String>();
+		
+		try {
+			TokenStream stream = analyzer.tokenStream(null, new StringReader(text));
+			stream.reset();
+			CharTermAttribute term = stream.getAttribute(CharTermAttribute.class);
+			
+			while(stream.incrementToken()) {
+				stemmer.setCurrent(term.toString());
+				stemmer.stem();
+				list.add(stemmer.getCurrent());
+				System.out.println(stemmer.getCurrent());
+			}
+			stream.close();
+		}
+		catch(IOException e){
+			throw new RuntimeException(e);
+		}
+		return list;
 	}
+	
 
 	public void clean() {
+		analyzer.close();
 	}
 
 }
